fff8964e735d8646c626f28d92c65c00a9c9dcf9
DESKTOP-09H8KCK\bachu <bachuthoihian9x@gmail.com>
2022-10-19 22:17:50 +0700

    update message

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ const nameGirl = 'Em';
 const giftUrl = 'https://www.facebook.com/profile.php?id=100023591147240';
 const eventName = 'Chúc Mừng 20-10';
 const titleCard = 'Thúy Quỳnh';
-const contentCard = 'Chúc Quỳnh có ngày 20/10 vui vẻ, tràn ngập niềm vui bên gia đình và bạn bè';
+const contentCard = 'Chúc Quỳnh có ngày 20/10 vui vẻ, tràn ngập niềm vui bên gia đình và bạn bè nhé.';
 
 // phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
 // Cần hỗ trợ hãy liên hệ: 
